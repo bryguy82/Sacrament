070d1613b61709730544dffeb809da8f8d88a5ac
Bryan Anderson <bryguy82@hotmail.com>
2019-12-13 02:58:21 -0300

    Added models and database create page for meeting is far along.

---
+++ Models/Topic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sacrament.Models
+{
+    public class Topic
+    {
+        public int TopicId { get; set; }
+
+        [Required]
+        [Display(Name = "Subject")]
+        [StringLength(50)]
+        public string Subject { get; set; }
+    }
+}

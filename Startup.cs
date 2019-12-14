ef606074dbb1b9c6f487ecee1a86361d8247575b
Jeff Helsten <Jeffh@jane.com>
2019-12-14 08:57:34 -0700

    homepage first styles

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Sacrament.Models;
 
 namespace Sacrament
 {
@@ -26,9 +24,6 @@ namespace Sacrament
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddDbContext<SacramentContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SacramentContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

e9014d9acf0d6a8c22cafad689bedb23b123bec8
Greenway-Benjamin <benjamingreenway@gmail.com>
2019-12-18 15:38:59 -0700

    Revert "Revert "Added models and database""

---
+++ Controllers/HymnsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sacrament.Models;
+
+namespace Sacrament.Controllers
+{
+    public class HymnsController : Controller
+    {
+        private readonly SacramentContext _context;
+
+        public HymnsController(SacramentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Hymns
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Hymn.ToListAsync());
+        }
+
+        // GET: Hymns/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hymn = await _context.Hymn
+                .FirstOrDefaultAsync(m => m.HymnId == id);
+            if (hymn == null)
+            {
+                return NotFound();
+            }
+
+            return View(hymn);
+        }
+
+        // GET: Hymns/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hymns/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HymnId,HymnNum,Hymn_1,Hymn_2,Hymn_3,Hymn_4")] Hymn hymn)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hymn);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hymn);
+        }
+
+        // GET: Hymns/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hymn = await _context.Hymn.FindAsync(id);
+            if (hymn == null)
+            {
+                return NotFound();
+            }
+            return View(hymn);
+        }
+
+        // POST: Hymns/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HymnId,HymnNum,Hymn_1,Hymn_2,Hymn_3,Hymn_4")] Hymn hymn)
+        {
+            if (id != hymn.HymnId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hymn);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HymnExists(hymn.HymnId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hymn);
+        }
+
+        // GET: Hymns/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hymn = await _context.Hymn
+                .FirstOrDefaultAsync(m => m.HymnId == id);
+            if (hymn == null)
+            {
+                return NotFound();
+            }
+
+            return View(hymn);
+        }
+
+        // POST: Hymns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hymn = await _context.Hymn.FindAsync(id);
+            _context.Hymn.Remove(hymn);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HymnExists(int id)
+        {
+            return _context.Hymn.Any(e => e.HymnId == id);
+        }
+    }
+}
